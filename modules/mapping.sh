9c7946f1528e9cfded313a6836f5e6a82889818e
Ubuntu <fiona.hak@universite-paris-saclay.fr>
2023-10-16 09:47:56 +0000

    finish pipeline

--- modules/mapping.sh
+++ modules/mapping.sh
@@ -3,17 +3,19 @@
 FILTERED_FASTQ="results/FASTQ/FILTERED"
 MAPPING="results/MAPPING"
 MAPPING_REF="results/DATA_REF"
-MPILEUP="results/MPILEUP"
 
 echo "Download reference and annotation file ............................. 0%"
 
 #créer le répertoire si nécessaire
 if [ ! -d results/MAPPING ];then
   mkdir -p $MAPPING_REF
+  mkdir -p $MAPPING
 fi
 
 wget -P $MAPPING_REF http://hgdownload.soe.ucsc.edu/goldenPath/hg19/chromosomes/chr16.fa.gz > /dev/null 2>&1
-wget -P $MAPPING_REF ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_24/GRCh37_mapping/gencode.v24lift37.basic.annotation.gtf.gz > /dev/null 2>&1
+echo -e "\033[1ADownload reference and annotation file ............................. 30%"
+wget -P $MAPPING_REF https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_24/GRCh37_mapping/gencode.v24lift37.basic.annotation.gtf.gz > /dev/null 2>&1
+echo -e "\033[1ADownload reference and annotation file ............................. 60%"
 gunzip $MAPPING_REF/gencode.v24lift37.basic.annotation.gtf.gz > /dev/null 2>&1
 echo -e "\033[1ADownload reference and annotation file ............................. 100%"
 
@@ -25,7 +27,7 @@ echo "Mapping and process alignement files ............................. 0%"
 sample_list=("TCRBOA7-N-WEX-chr16" "TCRBOA7-T-WEX-chr16")
 x=0
 for i in ${sample_list[*]};do
-  ((x=x+15))
+  ((x=x+30))
   echo -e "\033[1AMapping and process alignement files ............................. ${x}%"
   bwa mem -M -t 2 -A 2 -E 1 $MAPPING_REF/chr16.fa.gz $FILTERED_FASTQ/${i}_1P.fastq $FILTERED_FASTQ/${i}_2P.fastq -o $MAPPING/${i}.sam > /dev/null 2>&1
   samtools view -S -b $MAPPING/${i}.sam -o $MAPPING/${i}.bam > /dev/null 2>&1
@@ -34,20 +36,3 @@ for i in ${sample_list[*]};do
   samtools index $MAPPING/${i}_sorted.bam > /dev/null 2>&1
 done
 echo -e "\033[1AMapping and process alignement files ............................. 100%"
-
-echo "Pileup files ............................. 0%"
-
-#créer le répertoire si nécessaire
-if [ ! -d results/MPILEUP ];then
-  mkdir -p $MPILEUP
-fi
-
-gunzip $MAPPING_REF/chr16.fa.gz
-sample_list=("TCRBOA7-N-WEX-chr16" "TCRBOA7-T-WEX-chr16")
-x=0
-for i in ${sample_list[*]};do
-  ((x=x+15))
-  echo -e "\033[1APileup files ............................. ${x}%"
-  samtools mpileup -B -A -f $MAPPING_REF/chr16.fa $MAPPING/${i}_sorted.bam -o $MPILEUP/${i}.bcf > /dev/null 2>&1
-done
-echo -e "\033[1APileup files ............................. 100%"
--- modules/variant_calling.sh
+++ modules/variant_calling.sh
@@ -1,23 +1,42 @@
 #!/bin/bash
 
-MPILEUP="results/MPILEUP"
-VARIANT_CALLING="results/VARIANT_CALLING"
+MPILEUP="results/VARIANT_CALLING"
+VARIANT_CALLING="results/PROCESS_VARIANT_CALLING"
 MAPPING_REF="results/DATA_REF"
+MAPPING="results/MAPPING"
 
 #créer le répertoire si nécessaire
 if [ ! -d results/VARIANT_CALLING ];then
   mkdir -p $VARIANT_CALLING
 fi
 
+echo "Mpileup ............................. 0%"
+
+#créer le répertoire si nécessaire
+if [ ! -d results/MPILEUP ];then
+  mkdir -p $MPILEUP
+fi
+
+gunzip $MAPPING_REF/chr16.fa.gz
+sample_list=("TCRBOA7-N-WEX-chr16" "TCRBOA7-T-WEX-chr16")
+x=0
+for i in ${sample_list[*]};do
+  ((x=x+15))
+  echo -e "\033[1AMpileup ............................. ${x}%"
+  samtools mpileup -B -A -f $MAPPING_REF/chr16.fa $MAPPING/${i}_sorted.bam -o $MPILEUP/${i}.vcf > /dev/null 2>&1
+done
+echo -e "\033[1AMpileup ............................. 100%"
+
 echo "Variant calling ............................. 0%"
-varscan somatic $MPILEUP/TCRBOA7-N-WEX-chr16.bcf $MPILEUP/TCRBOA7-T-WEX-chr16.bcf $VARIANT_CALLING/TCRBOA7_compare.vcf > /dev/null 2>&1
+varscan somatic $MPILEUP/TCRBOA7-N-WEX-chr16.vcf $MPILEUP/TCRBOA7-T-WEX-chr16.vcf $VARIANT_CALLING/TCRBOA7_compare > /dev/null 2>&1
 echo -e "\033[1AVariant calling ............................. 100%"
 
 echo "Annotation VCF ............................. 0%"
-grep -i 'somatic' $VARIANT_CALLING/TCRBOA7_compare.vcf > $VARIANT_CALLING/TCRBOA7_compare_filtered.vcf
-awk '{OFS="\t"; if (!/^#/){print $1,$2-1,$2,$4"/"$5,"+"}}' $VARIANT_CALLING/TCRBOA7_compare_filtered.vcf > $VARIANT_CALLING/TCRBOA7_compare_filtered.bed
+join $VARIANT_CALLING/TCRBOA7_compare.snp $VARIANT_CALLING/TCRBOA7_compare.indel > $VARIANT_CALLING/TCRBOA7_joined.vcf
+grep -i 'somatic' $VARIANT_CALLING/TCRBOA7_joined.vcf > $VARIANT_CALLING/TCRBOA7_filtered.vcf
+awk '{OFS="\t"; if (!/^#/){print $1,$2-1,$2,$4"/"$5,"+"}}' $VARIANT_CALLING/TCRBOA7_filtered.vcf > $VARIANT_CALLING/TCRBOA7_filtered.bed
 echo -e "\033[1AAnnotation VCF ............................. 50%"
-bedtools intersect -a $MAPPING_REF/gencode.v24lift37.basic.annotation.gtf -b $VARIANT_CALLING/TCRBOA7_compare_filtered.bed > $VARIANT_CALLING/TCRBOA7_intersect.txt /dev/null 2>&1
-grep '\sgene\s' $VARIANT_CALLING/TCRBOA7_intersect.txt | awk '{print " " $1 " " $4 " " $5 " " $16}'
+bedtools intersect -a $MAPPING_REF/gencode.v24lift37.basic.annotation.gtf -b $VARIANT_CALLING/TCRBOA7_filtered.bed > $VARIANT_CALLING/TCRBOA7_intersect.txt /dev/null 2>&1
+grep '\sgene\s' $VARIANT_CALLING/TCRBOA7_intersect.txt | awk '{print " " $1 " " $4 " " $5 " " $16}' > $VARIANT_CALLING/final_genes.txt
 
 
